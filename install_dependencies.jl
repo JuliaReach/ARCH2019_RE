3e4eeccf5df46f82469b5771a30acd1bdde346c1
schillic <schillic@informatik.uni-freiburg.de>
2019-04-06 17:29:45 +0200

    add Manifest for installation

--- install_dependencies.jl
+++ install_dependencies.jl
@@ -2,27 +2,15 @@
 install and precompile required Julia packages
 =============================================#
 
-using Pkg
-
 # --- installation ---
 
+# install released packages with their exact versions
+import Pkg
+Pkg.activate(@__DIR__)
+Pkg.instantiate()
+
+# install unreleased packages
 Pkg.add([
-# fix important external libraries
-    PackageSpec(name="Polyhedra", version="0.4.5"),
-    PackageSpec(name="MAT", version="0.5.0"),
-    PackageSpec(name="HybridSystems", version="0.1.4"),
-    PackageSpec(name="TaylorModels", version="0.1.0"),
-    PackageSpec(name="TaylorSeries", version="0.9.1"),
-
-# additional packages for benchmarking and plotting
-    PackageSpec(name="BenchmarkTools", version="0.4.2"),
-    PackageSpec(name="Plots", version="0.24.0"),
-    PackageSpec(name="GR", version="0.38.1"),
-    PackageSpec(name="LaTeXStrings", version="1.0.3"),
-
-# JuliaReach packages
-    PackageSpec(name="MathematicalSystems", version="0.6.4"),
-    PackageSpec(name="LazySets", version="1.10.0"),
     PackageSpec(url="https://github.com/JuliaReach/Reachability.jl",
                 rev="cb7a0b32e8423ef103539cbda849fa1fff7af2ce"),
     PackageSpec(url="https://github.com/JuliaReach/SX.jl",
@@ -34,6 +22,7 @@ Pkg.add([
 import Reachability
 import SX
 import Polyhedra
+import Optim
 import MAT
 import BenchmarkTools
 import Plots
